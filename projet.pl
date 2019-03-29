4d63c6798976f1228278e4db0f2a3583c76d825b
Charlotte André <charlotte-andre@hotmail.fr>
2019-03-29 11:31:23 +0100

    Maj perl test!

--- projet.pl
+++ projet.pl
@@ -2,7 +2,9 @@ my $dbh = DBI->connect("DBI:Pg:dbname=alaporte006;host=dbserver","alaporte006","
 
 
 open(EnsemblPlant, 'mart_export.csv') ; #ouverture en lecture seule
-open(Uniprot, 'uniprot-arabidopsis+thaliana.tab.txt')
+open(EnsemblPLanttrier, '>mart_exporttri.csv') ;
+open(Uniprot, 'uniprot-arabidopsis+thaliana.tab.txt');
+open(Uniprottri, '>Uniprottri.tab.txt') ;
 
 
 ######Table EnsemblPlant
@@ -11,8 +13,9 @@ while (<EnsemblPlant>) {
   @tmp= split(/,/,$_);
   $dupli=1;
   if(@tmp[2]){
-    if (@tmp[3]{
-      $dbh+=$dbh "Insert in table" (@tmp[0],@tmp[1],@tmp[2],@tmp[3])
+    if (@tmp[3]){
+    #  $dbh+=$dbh
+      print FICHIER "@tmp[0],@tmp[1],@tmp[2],@tmp[3]";
 
 
     }
@@ -24,10 +27,10 @@ while (<EnsemblPlant>) {
 }
 
 
-while(<Uniprot>){
-  chomp;
-  @tmp= split(/\t/,$_);
-  if (@tmp[4]=~/(.*)<Arabidopsis thaliana>(.*)/){
-    INSERT table 
-  }
-}
+# while(<Uniprot>){
+#   chomp;
+#   @tmp= split(/\t/,$_);
+#   if (@tmp[4]=~/(.*)<Arabidopsis thaliana>(.*)/){
+#     INSERT table
+#   }
+# }
