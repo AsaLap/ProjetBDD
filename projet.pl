79c1ce904576defb16fd73819c64add5c9288d59
Charlotte Andre <chaandre@emi.u-bordeaux1.fr>
2019-03-29 17:15:28 +0100

    Insert fin

--- projet.pl
+++ projet.pl
@@ -10,43 +10,51 @@ my $dbh = DBI->connect("DBI:Pg:dbname=alaporte006;host=dbserver","alaporte006","
 
 open(EnsemblPlant, 'mart_export.csv') ; #ouverture en lecture seule
 open(csvtest, '>mart_exporttri.csv') ;
-open(Uniprot, 'Uniprot.txt');
+open(Uniprot, 'Uniprot.tab.txt');
 open(testtab, '>Uniprottri.tab.txt') ;
 
 my @tmp;
 my @tmp2;
 my $dupli;
 my @list_keys;
+my @list_keys2;
+my @list_keys3;
 
-# ######Table EnsemblPlant
-# while (<EnsemblPlant>) {
+#####Table EnsemblPlant
+while (<EnsemblPlant>) {
+  chomp;
+  @tmp= split(/,/,$_);
+  $dupli=1;
+  if(@tmp[2]){
+    if(join(" ",@list_keys)=~/$tmp[2]/){
+      $dbh+=$dbh;
+    }
+    else{
+      push @list_keys, @tmp[2];
+      if (@tmp[3]){
+        $dbh+=$dbh;
+        print csvtest "@tmp[0],@tmp[1],@tmp[2],@tmp[3]\n";
+      }
+      else {
+        print csvtest "@tmp[0],@tmp[1],@tmp[2],  \n";
+      }
+    }
+  }
+}
+
+# while(<Uniprot>){
 #   chomp;
-#   @tmp= split(/,/,$_);
-#   $dupli=1;
-#   if(@tmp[2]){
-#     if(join(" ",@list_keys)=~/$tmp[2]/){
+#   $_=~tr/\'/ /;
+#   @tmp2= split(/\t/,$_);
+#   if (@tmp2[5]=~/.*Arabidopsis thaliana.*/) {
+#     if(join(" ",@list_keys2)=~/$tmp2[0]/){
 #       $dbh+=$dbh;
 #     }
 #     else{
-#       push @list_keys, @tmp[2];
-#       if (@tmp[3]){
-#         $dbh+=$dbh;
-#         print csvtest "@tmp[0],@tmp[1],@tmp[2],@tmp[3]\n";
-#       }
-#       else {
-#         print csvtest "@tmp[0],@tmp[1],@tmp[2],  \n";
-#       }
+#       push @list_keys2,$tmp[0];
+#       push @list_keys3,$tmp[4];
+#       print testtab "@tmp2[0]\t@tmp2[1]\t@tmp2[2]\t@tmp2[3]\t@tmp2[4]\t@tmp2[6]\t@tmp2[7]\t@tmp2[8]\t@tmp2[9]\t@tmp2[10]\n";
+#       $dbh+=$dbh;
 #     }
 #   }
 # }
-
-while(<Uniprot>){
-  chomp;
-  @tmp2= split(/\t/,$_);
-  if (@tmp2[5]=~/.*Arabidopsis thaliana.*/) {
-    $dbh+=$dbh;
-    print testtab "@tmp2[0]\t@tmp2[1]\t@tmp2[2]\t@tmp2[3]\t@tmp2[4]\t@tmp2[6]\n"
-
-  }
-  # print testtab "@tmp2[0] @tmp2[1]  @tmp2[2]  @tmp2[3]  @tmp2[5]  @tmp2[6]\n"
-}
