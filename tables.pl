0afc13d058365cd489b1685672bec94f6346a2ed
AsaLap <45365864+AsaLap@users.noreply.github.com>
2019-04-03 18:03:09 +0200

    Delete tables.pl

--- tables.pl
+++
@@ -1,6 +0,0 @@
-#!bin/usr/perl
-
-$dbh->do("create table UniProt(UniProtKB_TrEMBL_ID varchar primary key, GeneStableID varchar, TranscriptStableID varchar, PlantReactionID varchar)");
-$dbh->do("create table General(Entry varchar primary key, EntryName varchar, Status in ("reviewed", "unreviewed"), Organism varchar, GeneName varchar, ProteinName varchar, EnsemblProt varchar)");
-$dbh->do("create table Protein(ProteinName varchar primary key, Sequence varchar, Length int)");
-$dbh->do("create table Gene(GeneName varchar primary key, Synonym varchar, GeneOnthology varchar)");
